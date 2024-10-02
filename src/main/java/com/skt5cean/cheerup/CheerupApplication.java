2067d21f090202b4c3f8502e856677a9eb65510b
Jiwon Kwon <kjiwon411@gmail.com>
2024-10-03 00:31:08 +0900

    [feat] 웹 클라이언트 추가

--- src/main/java/com/skt5cean/cheerup/CheerupApplication.java
+++ src/main/java/com/skt5cean/cheerup/CheerupApplication.java
@@ -2,6 +2,8 @@ package com.skt5cean.cheerup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CheerupApplication {
@@ -10,4 +12,8 @@ public class CheerupApplication {
 		SpringApplication.run(CheerupApplication.class, args);
 	}
 
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder().build();
+	}
 }
