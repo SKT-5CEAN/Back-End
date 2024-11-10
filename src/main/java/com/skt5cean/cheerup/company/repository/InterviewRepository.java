ce1c23cb14f07865eb77aa094b2e06b54770247c
Jiwon Kwon <kjiwon411@gmail.com>
2024-11-10 23:09:49 +0900

    [feat] interview repository에 찾기 기능 추가

--- src/main/java/com/skt5cean/cheerup/company/repository/InterviewRepository.java
+++ src/main/java/com/skt5cean/cheerup/company/repository/InterviewRepository.java
@@ -1,4 +1,12 @@
 package com.skt5cean.cheerup.company.repository;
 
-public class InterviewRepository {
+import com.skt5cean.cheerup.company.domain.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+    List<Interview> findAllByCompanyId(Long companyId);
+    List<Interview> findAllByCompanyIdAndState(Long companyId, Interview.StateType state);
 }
